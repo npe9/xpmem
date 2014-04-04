d9ce13ad84acb7c5c71354e68be01c1747977824
Brian Kocoloski <briankoco@cs.pitt.edu>
2014-04-04 19:59:49 -0400

    fucking deadlock

--- xpmem_attach.c
+++ xpmem_attach.c
@@ -519,10 +519,10 @@ xpmem_detach(u64 at_vaddr)
 	att = (struct xpmem_attachment *)vma->vm_private_data;
 	if (!xpmem_is_vm_ops_set(vma) || att == NULL) {
         ret = -EINVAL;
+        up_write(&current->mm->mmap_sem);
         if (extend_enabled) {
             ret = xpmem_extended_ops->detach(xpmem_my_part, at_vaddr);
         }
-        up_write(&current->mm->mmap_sem);
 		return ret;
 	}
 	xpmem_att_ref(att);
