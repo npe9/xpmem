452e0be222a17b364a32b74a74ded071d946d498
Brian Kocoloski <briankoco@cs.pitt.edu>
2014-06-06 18:27:10 -0400

    hacky symbol lookups

---
+++ include/xpmem_syms.h
@@ -0,0 +1,17 @@
+/*
+ * XPMEM extensions for multiple domain support
+ *
+ * xpmem_syms.h: Linux kernel symbols needed for module functionality
+ *
+ * Author: Brian Kocoloski <briankoco@cs.pitt.edu>
+ *
+ */
+
+#ifndef _XPMEM_SYMS_H
+#define _XPMEM_SYMS_H
+
+
+int xpmem_linux_symbol_init(void);
+
+
+#endif
