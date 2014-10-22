5a33903314c2363209a7ce0e4b864ca461db84c6
Brian Kocoloski <briankoco@cs.pitt.edu>
2014-10-22 14:10:12 -0400

    updated test

--- test/test_xpmem.c
+++ test/test_xpmem.c
@@ -4,22 +4,32 @@
 
 #include <xpmem.h>
 
+#define PAGE_SIZE sysconf(_SC_PAGESIZE)
+
 int main(int argc, char ** argv) {
     int * addr;
     xpmem_segid_t segid;
+    int num_pages;
 
-    if (posix_memalign((void **)&addr, sysconf(_SC_PAGESIZE), sizeof(int)) != 0) {
+    if (argc != 2) {
+        printf("Usage: %s <num_pages>\n", *argv);
+        return -1;
+    }
+
+    num_pages = atoi(*(++argv));
+
+    if (posix_memalign((void **)&addr, PAGE_SIZE, PAGE_SIZE * num_pages) != 0) {
         perror("posix_memalign");
         return -1;
     }
 
-    segid = xpmem_make((void *)addr, 4096 * 10, XPMEM_PERMIT_MODE, (void *)0600);
+    segid = xpmem_make((void *)addr, PAGE_SIZE * num_pages, XPMEM_PERMIT_MODE, (void *)0600);
     printf("segid: %lli\n", segid);
 
     {
         int i = 0;
-        for (i = 0; i < 10; i++) {
-            void * addr2 = ((void *)addr + (4096 * i));
+        for (i = 0; i < num_pages; i++) {
+            void * addr2 = ((void *)addr + (PAGE_SIZE * i));
             *((int *)addr2) = 12340 + i;
             printf("addr: %p, *addr: %d\n",
                 addr2, *((int *)addr2));
